644c65d5f545bdcf8f4dc42988ac78b153581371
varvarapod <varvara.podovzhnia@gmail.com>
2024-04-12 17:44:44 +0100

    -add visualisations

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
 
-from flask import (Flask, redirect, render_template, request,
+import pandas as pd
+from flask import (Flask, redirect, render_template, request, session,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
 
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def index():
@@ -27,6 +30,50 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+# Route for receiving user preferences and providing movie suggestions
+@app.route('/movielist', methods=['GET', 'POST'])
+def movielist():
+    # Get user preferences from the form
+    # genre = request.form.get('genre')
+    # min_rating = float(request.form.get('min_rating'))
+    name = request.form.get('name')
+    page = int(request.args.get('page', 1))
+    per_page = 1
+
+    if name:
+        name = name.lower()
+        session['moviesearch'] = name
+    elif 'moviesearch' in session:
+        name = session['moviesearch']
+    else:
+        return redirect(url_for('index'))
+
+
+    # Search for movies based on user preferences
+    #movies = ia.search_movie(genre)
+    movie_db_path = 'data/imdb_mini.csv'
+    df = pd.read_csv(movie_db_path)
+    movies = df[df['title'].str.lower().str.contains("(?i)" + name)]
+
+    movies_list = []
+    for index, row in movies.head(4).iterrows():
+        movies_list.append({
+            'title': row['title'],
+            'rating': row['rating'],
+            'image': row.get('poster_url'),
+            'summary': row.get('plot'),
+        })
+
+
+
+    # Pagination
+    start_index = (page - 1) * per_page
+    end_index = start_index + per_page
+    num_pages = (len(movies_list) + per_page - 1) // per_page
+    movies_list = movies_list[start_index:end_index]
+
+    # Render the suggestions template with the movie suggestions and pagination data
+    return render_template('list.html', movies_list=movies_list, page=page, num_pages=num_pages)
 
 if __name__ == '__main__':
    app.run()
